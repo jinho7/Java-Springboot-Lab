d2a28296bbf1355283245f6f58fc4b2917c23280
jinho7 <69239293+jinho7@users.noreply.github.com>
2025-02-05 00:12:26 +0900

    first commit

---
+++ src/main/java/com/example/Java_Springboot_Lab/JavaSpringbootLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.Java_Springboot_Lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Java_Springboot_Lab/JavaSpringbootLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Java_Springboot_Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
